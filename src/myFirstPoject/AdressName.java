1fba6f28c0606166a7eed431e24e72d11a34efce
kat <kate.lebid@gmail.com>
2016-03-16 22:37:29 +0200

    kkk

---
+++ src/myFirstPoject/AdressName.java
@@ -0,0 +1,20 @@
+package myFirstPoject;
+
+public class AdressName {
+
+	public static void main(String[] args) {
+		String name = "Kateryna Лебедь";
+		String city = "Киев, Украина";
+		String street = "Шолом-Алейхема 6";
+		//String name = "Kateryna Лебедь";
+		System.out.println(name);
+		System.out.println(city);
+		System.out.println(street);
+		
+		System.out.printf("ФИО: %s  Город/Страна: %s Улица: %s ", name, city, street);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/myFirstPoject/HelloWorld.java
@@ -0,0 +1,12 @@
+package myFirstPoject;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi world from eclipse" 
+				+ "adress"
+				);
+	}
+
+}
